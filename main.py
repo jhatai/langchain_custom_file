5f396ac2b8b3e8acb8430b3bf7ea3a42c8f17c23
mach9github <87606158+mach9github@users.noreply.github.com>
2023-05-08 17:17:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -103,31 +103,24 @@ with st.form(key='import_form'):
 
 
 
-st.write("""
-# File Picker
-""")
-uploaded_file = st.file_uploader("Choose a PDF file")
-# if uploaded_file is not None:
-#     bytes_data = uploaded_file.getvalue()
-#     data = uploaded_file.getvalue().decode('utf-8').splitlines()         
-#     st.session_state["preview"] = ''
-#     for i in range(0, min(5, len(data))):
-#         st.session_state["preview"] += data[i]
-# preview = st.text_area("CSV Preview", "", height=150, key="preview")
-# upload_state = st.text_area("Upload State", "", key="upload_state")
-def upload():
-    if uploaded_file is None:
-        st.session_state["upload_state"] = "Upload a file first!"
-    else:
-        # data = uploaded_file.getvalue().decode('utf-8')
-        parent_path = pathlib.Path(__file__).parent.parent.resolve()           
-        save_path = os.path.join(parent_path, "data")
-        complete_name = os.path.join(save_path, uploaded_file.name)+"/"+uploaded_file.name
+# st.write("""
+# # File Picker
+# """)
+# uploaded_file = st.file_uploader("Choose a PDF file")
+
+# def upload():
+#     if uploaded_file is None:
+#         st.session_state["upload_state"] = "Upload a file first!"
+#     else:
+#         # data = uploaded_file.getvalue().decode('utf-8')
+#         parent_path = pathlib.Path(__file__).parent.parent.resolve()           
+#         save_path = os.path.join(parent_path, "data")
+#         complete_name = os.path.join(save_path, uploaded_file.name)+"/"+uploaded_file.name
         
-        with open(complete_name, "wb") as destination_file:
-            destination_file.write(uploaded_file.getbuffer())
-            destination_file.close()
-            st.session_state["upload_state"] = "Saved " + complete_name + " successfully!"
-            st.session_state["file_name"] = uploaded_file.name
-    # st.write(st.session_state["file_name"])
-st.button("Upload file to Sandbox", on_click=upload)
+#         with open(complete_name, "wb") as destination_file:
+#             destination_file.write(uploaded_file.getbuffer())
+#             destination_file.close()
+#             st.session_state["upload_state"] = "Saved " + complete_name + " successfully!"
+#             st.session_state["file_name"] = uploaded_file.name
+#     # st.write(st.session_state["file_name"])
+# st.button("Upload file to Sandbox", on_click=upload)
