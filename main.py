0b04a4715bc02ad496336554799b2efefb884f6d
mach9github <87606158+mach9github@users.noreply.github.com>
2023-05-08 17:10:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -119,12 +119,15 @@ def upload():
     if uploaded_file is None:
         st.session_state["upload_state"] = "Upload a file first!"
     else:
-        data = uploaded_file.getvalue().decode('utf-8')
+        # data = uploaded_file.getvalue().decode('utf-8')
         parent_path = pathlib.Path(__file__).parent.parent.resolve()           
         save_path = os.path.join(parent_path, "data")
-        complete_name = os.path.join(save_path, uploaded_file.name)
-        destination_file = open(complete_name, "w")
-        destination_file.write(data)
-        destination_file.close()
-        st.session_state["upload_state"] = "Saved " + complete_name + " successfully!"
+        complete_name = os.path.join(save_path, uploaded_file.name)+"/"+uploaded_file.name
+        
+        with open(complete_name, "wb") as destination_file:
+            destination_file.write(uploaded_file.getbuffer())
+            destination_file.close()
+            st.session_state["upload_state"] = "Saved " + complete_name + " successfully!"
+            st.session_state["file_name"] = uploaded_file.name
 st.button("Upload file to Sandbox", on_click=upload)
+st.write(st.session_state["file_name"])
