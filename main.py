c08e1235964271b93eceb3ab22d5315a8b55cd63
mach9github <87606158+mach9github@users.noreply.github.com>
2023-05-08 16:48:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain import OpenAI, VectorDBQA
+import os.path
+import pathlib
 
+
+uploaded_files = st.file_uploader("Choose a pdf file", accept_multiple_files=False)
+
+bytes_data = uploaded_file.read()
+st.write("filename:", uploaded_file.name)
+# st.write(bytes_data)
 
 # import pdf
 # Form+ filepicker + commit + text with status
@@ -94,6 +102,32 @@ with st.form(key='import_form'):
     import_submit_button = st.form_submit_button(
         label='Submit', on_click=import_file)
 
-  
-st.text_input(label="test state", key='test')
-st.write(st.session_state.test)
+
+
+
+
+st.write("""
+# File Picker
+""")
+uploaded_file = st.file_uploader("Choose a PDF file")
+# if uploaded_file is not None:
+#     bytes_data = uploaded_file.getvalue()
+#     data = uploaded_file.getvalue().decode('utf-8').splitlines()         
+#     st.session_state["preview"] = ''
+#     for i in range(0, min(5, len(data))):
+#         st.session_state["preview"] += data[i]
+# preview = st.text_area("CSV Preview", "", height=150, key="preview")
+# upload_state = st.text_area("Upload State", "", key="upload_state")
+def upload():
+    if uploaded_file is None:
+        st.session_state["upload_state"] = "Upload a file first!"
+    else:
+        data = uploaded_file.getvalue().decode('utf-8')
+        parent_path = pathlib.Path(__file__).parent.parent.resolve()           
+        save_path = os.path.join(parent_path, "data")
+        complete_name = os.path.join(save_path, uploaded_file.name)
+        destination_file = open(complete_name, "w")
+        destination_file.write(data)
+        destination_file.close()
+        st.session_state["upload_state"] = "Saved " + complete_name + " successfully!"
+st.button("Upload file to Sandbox", on_click=upload)
