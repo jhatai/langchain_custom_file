6fec827be2542960f7722661a42844da3eb6bf2a
mach9github <87606158+mach9github@users.noreply.github.com>
2023-05-08 14:22:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -52,9 +52,10 @@ def get_human_text():
 
 def show_human_search_form(faiss_index):
     with st.form(key='human_form'):
-        similarity_text = get_human_text()
+        human_question = get_human_text()
+        print(f"human_question is {human_question}")
         similarity_submit_button = st.form_submit_button(
-            label='Submit', on_click=query_human, args=[similarity_text, faiss_index])
+            label='Submit', on_click=query_human, args=[human_question, faiss_index])
 
 
 def import_file():
